da05a45c66ee9ae5c9e703affe200b402359a3a2
paulo ricardo <pauloricardojm@hotmail.com.br>
2018-05-29 01:25:47 -0300

    Player implemetation

---
+++ player.cpp
@@ -0,0 +1,34 @@
+#include "inc/player.hpp"
+
+Player::Player(string name)
+{
+    this->name = name;
+    this->mao.reserve(3);
+}
+
+void Player::set_cards(Card c)
+{
+    if(this->mao.size() < 3)
+    {
+        this->mao.push_back(c);
+    }
+}
+
+Card Player::get_card(unsigned int position)
+{
+    Card c;
+    if(this->mao.size() > 0)
+    {
+        if(position >= 0 && position < 3)
+        {
+            c = this->mao[position];
+            this->mao.erase(this->mao.begin() + position);
+        }
+    }
+    return c;
+}
+
+Player::~Player()
+{
+    this->mao.clear();
+}
