62792a7378411748098f956ae4457951d6feaea6
paulo ricardo <pauloricardojm@hotmail.com.br>
2018-06-21 17:42:02 -0300

    Overloaded stream operator >>. Added some functions too

--- inc/team.hpp
+++ inc/team.hpp
@@ -1,7 +1,9 @@
 #ifndef TEAM_HPP
     #define TEAM_HPP
-    
+        #include <iostream>
         #include "player.hpp"
+
+        using namespace std;
 
         class Team
         {
@@ -10,9 +12,12 @@
                 unsigned int points;
 
             public:
+                Team();
                 Team(string player1_name, string player2_name);
                 void set_points(const unsigned int points);
                 unsigned int get_points();
+                void display_player_name(unsigned int player_num);
+                friend istream &operator>>(istream &is, Team &obj);
                 ~Team();
         };
 #endif
