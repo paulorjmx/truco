d8f7228c7cd76ad1d38a2138453ab2c44d274c4d
paulo ricardo <pauloricardojm@hotmail.com.br>
2018-06-22 15:50:49 -0300

    Added function to create a new deck

--- deck.cpp
+++ deck.cpp
@@ -15,6 +15,22 @@ Deck::Deck()
     }
 }
 
+void Deck::create()
+{
+    this->shuffled = false;
+    this->deck.clear();
+    this->deck.reserve(40);
+    char symbols[10] = { 'A', '2', '3', '4', '5', '6', '7', 'Q', 'J', 'K' };
+    char naipes[4] = { 'c', 'e', 'o', 'p'}; // Copa, Espada, Ouros e Paus
+    for(int i = 0; i < 10; i++)
+    {
+        for(int j = 0; j < 4; j++)
+        {
+            this->deck.push_back(Card(symbols[i], naipes[j]));
+        }
+    }
+}
+
 void Deck::print_deck()
 {
     for(vector<Card>::iterator it = this->deck.begin(); it < this->deck.end(); it++)
