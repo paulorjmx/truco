ed223c43853c77bc22a854b7f3a92eb0adb48e02
paulo ricardo <pauloricardojm@hotmail.com.br>
2018-06-21 23:26:57 -0300

    Implemented card player manipulation

--- team.cpp
+++ team.cpp
@@ -12,9 +12,19 @@ Team::Team(string player1_name, string player2_name)
     this->points = 0;
 }
 
-void Team::set_points(const unsigned int points)
+void Team::set_points(unsigned int points)
 {
     this->points = points;
+}
+
+void Team::set_card(unsigned int player_num, Card c)
+{
+    this->players[player_num].set_cards(c);
+}
+
+Card Team::get_card(unsigned int player_num, unsigned int card_position)
+{
+    return this->players[player_num].get_card(card_position);
 }
 
 unsigned int Team::get_points()
