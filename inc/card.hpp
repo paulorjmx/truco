e17dee793e6c7dec82f7fc3a968536cfcdc6b0e0
paulo ricardo <pauloricardojm@hotmail.com.br>
2018-06-23 16:42:01 -0300

    Added enum Naipe type

--- inc/card.hpp
+++ inc/card.hpp
@@ -3,15 +3,28 @@
 
         class Card
         {
-            private:
-                char symbol, naipe;
-
             public:
+                enum Naipe
+                {
+                    PAUS = 4,
+                    COPAS = 3,
+                    ESPADA = 2,
+                    OUROS = 1
+                };
                 Card();
-                Card(char symbol, char naipe);
+                Card(char symbol, Naipe naipe);
                 Card(const Card &card);
-                char get_symbol();
-                char get_naipe();
+                void clear();
+                char print_symbol();
+                short int get_symbol();
+                Naipe get_naipe();
+                char print_naipe();
+                bool operator==(const Card &c) const;
                 ~Card();
+                
+            private:
+                short int symbol;
+                Naipe naipe;
+
         };
 #endif
